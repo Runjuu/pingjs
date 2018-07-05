c3731fed300328296ba2be6ace56d9cfe79637fb
Jehon <jehon@users.noreply.github.com>
2018-07-05 08:33:39 +0200

    Simplify writing
    Stop using indirect function
    Do not show error inside published function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // See LICENSE for usage information
 // Thanks to https://github.com/jdfreder/pingjs
+// Thanks to https://github.com/Runjuu/web-ping
 
 /**
  * Creates and loads an image element by url.
  * @param  {String} url
- * @param  {Number} timeout
+ * @param  {Number} timeout (ms)
  * @return {Promise} promise that resolves to an image element or
  *                   fails to an Error.
  */
@@ -21,19 +22,20 @@ const requestImage = (url, timeout) => (new Promise(function(resolve, reject) {
 /**
  * Pings a url.
  * @param  {String} url
- * @param  {Number} timeout
+ * @param  {Number} timeout (ms)
  * @return {Promise} promise that resolves to a ping (ms, float).
  */
 async function ping(url, timeout) {
   const start = (new Date()).getTime();
-
-  try {
-    await requestImage(url, timeout);
-  } catch (err) {
-    console.log(err);
-  	throw err;
-  }
-
+  await new Promise(function(resolve, reject) {
+	const img = new Image();
+	img.onload = resolve;
+	img.onerror = reject;
+	img.src = `${url}?random-no-cache=${Math.floor((1 + Math.random()) * 0x10000).toString(16)}`;
+  
+	clearTimeout(requestImage.timeout);
+	if (timeout) requestImage.timeout = setTimeout(() => reject(`Timeout after ${timeout} ms`), timeout);
+  });
   return (new Date()).getTime() - start;
 }
 
